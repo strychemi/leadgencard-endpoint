18c2d0a3fa3142db6ee2295e320ccd70b067de95
Jacob Petrie <jpetrie@twitter.com>
2013-11-07 11:06:43 -0800

    test app

--- web.rb
+++ web.rb
@@ -9,7 +9,10 @@ end
 post '/' do
   puts "POST request start"
   puts request["name"]
+  raw2 = request.body
   raw = request.env["rack.input"].read
   "request was: #{raw}"
+  "request was: #{raw2}"
+  puts raw2
   puts "POST request end"
 end
