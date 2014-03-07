29e46b19f5b5e7493becd65eb97ae197617389a6
Jacob Petrie <jpetrie@twitter.com>
2014-03-06 17:51:17 -0800

    more detail test

--- web.rb
+++ web.rb
@@ -1,14 +1,13 @@
 require 'sinatra'
-require 'logger'
 
 get '/' do
   "GET request"
   raw = request.env["rack.input"].read
   puts "\n\n request was: #{raw} \n\n"
-  logger.info(request.env.inspect)
 end
 
 post '/' do
   puts "POST start: " + request["name"] + " | " + request["email"] + " | " + request["screen_name"] + " | " + request["token"] + " | " + request["card"] + " | " + request["testCustomHiddenField"] + " | POST end"
+  puts request.raw_post
 end
 
