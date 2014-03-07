617ab85e37e6af2e55e7edd89cbf3f1fbaac301b
Jacob Petrie <jpetrie@twitter.com>
2014-03-06 18:05:55 -0800

    more detail test

--- web.rb
+++ web.rb
@@ -10,6 +10,6 @@ post '/' do
   puts "POST start: " + request["name"] + " | " + request["email"] + " | " + request["screen_name"] + " | " + request["token"] + " | " + request["card"] + " | " + request["testCustomHiddenField"] + " | POST end"
 #  @rawpost = request.raw_post
 #  puts "@rawpost"
-  puts request.env['RAW_POST_DATA']
+  puts "POST RAW Begin:\n" + request.env['RAW_POST_DATA'] + "\nPOST RAW End"
 end
 
